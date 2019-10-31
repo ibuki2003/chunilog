fd2d4e46312af24f93b6c3ff9fe434a7bbd9a02b
ibuki2003 <ibuki426.sp@gmail.com>
2019-11-01 01:02:46 +0900

    fix method options

--- routes/statefulApi.php
+++ routes/statefulApi.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware(['chunithm_cors', 'auth'])->group(function () {
+Route::middleware('chunithm_cors')->group(function () {
     Route::options('new_records', 'RecordController@empty');
-    Route::post('new_records', 'RecordController@storeApi')->name('record.api');
+    Route::post('new_records', 'RecordController@storeApi')
+        ->middleware('auth')->name('record.api');
 });
